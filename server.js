dce0ac9f011a30a40c21db0a5d1f281d51f5099b
student <student@Admins-iMac.local>
2019-11-12 15:58:24 -0600

    need job

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const path = require('path')
+
+const app = express();
+const PORT = 3000;
+
+//Accessing 'public' folder
+app.use(express.static(path.join(__dirname, 'public'))
+);
+
+//logging url requests
+app.use('/', (req, res, next) => {
+  console.log(req.url);
+  next();
+})
+
+// Create express server that responds to root get req ('/')
+// with 'Hello from the server side
+app.use('/', (req, res) => {
+  console.log('Hello from the web server side')
+    }
+)
+
+//listen on port 3000
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+});      // I need a job please
